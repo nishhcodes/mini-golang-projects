e52eb010c5d21aab23d0eddf20cb56f9c4c24387
nishhcodes <issacneedscoffee@gmail.com>
2025-04-03 18:43:35 +0530

    ComicMischief project completed

--- ComicMischief/main.go
+++ ComicMischief/main.go
@@ -4,10 +4,27 @@ import "fmt"
 
 func main() {
 	var publisher, writer, artist, title string
+	title = "Mr. GoToSleep"
+	writer = "Tracey Hatchet"
+	artist = "Jewel Tampson"
+	publisher = "DizzyBooks Publishing Inc."
 
-	var year, pageNUmber int32
+	var year, pageNumber int32
+	year = 1997
+	pageNumber = 14
 
 	var grade float32
+	grade = 6.5
 
-	fmt.Println(title, "written by", writer, "drawn by", artist)
+	fmt.Println(title, "written by", writer, "drawn by", artist, "published by", publisher)
+	
+	title = "Epic Vol. 1"
+	writer = "Ryan N. Shawn"
+	artist = "Phoebe Paperclips"
+	publisher = "DizzyBooks Publishing Inc."
+	year = 2013
+	pageNumber = 160
+	grade = 9.0
+	
+	fmt.Println(title, "written by", writer, "drawn by", artist, "published by", publisher, pageNumber, "pages on the year", year, "and has", grade, "rating")
 }
