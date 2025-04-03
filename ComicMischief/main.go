2cf0179d3233275a04cc365d431f87c982bab3aa
nishhcodes <issacneedscoffee@gmail.com>
2025-04-03 18:32:08 +0530

    started the project ComicMischief

---
+++ ComicMischief/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var publisher, writer, artist, title string
+
+	var year, pageNUmber int32
+
+	var grade float32
+
+	fmt.Println(title, "written by", writer, "drawn by", artist)
+}
